d7d0057af694c2b9e8f3ce9f155036d5088cbf14
nvtank <tuannguyen.120910@gmail.com>
2025-10-23 12:39:53 +0700

    feat: auth function with Puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,11 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resume } from "react-dom/server";
 import { resumes } from "~/constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import {  useNavigate } from "react-router";
+import { useEffect } from "react";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
@@ -11,9 +14,16 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth} = usePuterStore();
+  const navigate = useNavigate();
+   
+      useEffect(() => {
+          if(!auth.isAuthenticated) navigate('/auth?next=/');
+      }, [auth.isAuthenticated]);
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Welcome to Resumind</h1>
